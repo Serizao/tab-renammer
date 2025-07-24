7e3f45707a7a030cb53fe76e0aecda4d7ade9d78
Serizao <william.leberre@gmail.com>
2025-07-24 09:20:56 +0200

    init action

--- caido.config.ts
+++ caido.config.ts
@@ -7,7 +7,7 @@ import tailwindCaido from "@caido/tailwindcss";
 import path from "path";
 import prefixwrap from "postcss-prefixwrap";
 
-const id = "frontend-vue";
+const id = "tab-renammer";
 export default defineConfig({
   id,
   name: "Tab Renamer",
