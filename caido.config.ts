57b636caf32051339e965700e874f8b89a00c7a0
Serizao <william.leberre@gmail.com>
2025-07-24 10:02:41 +0200

    Update caido.config.ts

--- caido.config.ts
+++ caido.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   id,
   name: "Tab Renamer",
   description: "Plugin used to rename automaticly tab in replay section",
-  version: "0.0.1",
+  version: "0.0.2",
   author: {
     name: "Serizao",
     email: "dev@caido.io",
